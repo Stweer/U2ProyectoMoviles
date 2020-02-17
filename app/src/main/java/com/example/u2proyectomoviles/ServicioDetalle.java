6800a7f6ac776ef9768f7f5b699766e0a8a98989
Stweer <38274756+Stweer@users.noreply.github.com>
2020-02-16 23:36:29 -0500

    Libreria para Detalle

---
+++ app/src/main/java/com/example/u2proyectomoviles/ServicioDetalle.java
@@ -0,0 +1,14 @@
+package com.example.u2proyectomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServicioDetalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicio_detalle);
+    }
+}
